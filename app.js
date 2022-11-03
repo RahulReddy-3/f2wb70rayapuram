2013e5ddc34316647cfd647a60a64a0d6c6a479c
RahulReddy-3 <iamrahulreddy.3@gmail.com>
2022-11-02 20:20:05 -0500

    Added GridBuild

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var vacuumcleanerRouter = require('./routes/vacuumcleaner');
+var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/vacuumcleaner', vacuumcleanerRouter);
+app.use('/gridbuild', gridbuildRouter);
 
 
 
